3083a0f2939f6ff360b3b920ea8112b1ccfd2ec4
generaldefence <anishg24@gmail.com>
2019-08-11 12:05:24 -0700

    works now

--- Extra Fun/battleship.py
+++ Extra Fun/battleship.py
@@ -33,28 +33,33 @@ def get_int(message):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
+last_message = False
+
 for each_turn in range(4):
-    with term.fullscreen():
-        term.move(0, 0)
-        print ("Turn", each_turn + 1)
-        print_board(board)
-        
-        guess_row = get_int("Guess Row: ")
-        guess_col = get_int("Guess Column: ")
-        
-        if guess_row == ship_row and guess_col == ship_col:
-            print ("Congrats, you won!")
-            break
-        else:
-            if (guess_row < 1 or guess_row > len(board)) or (guess_col < 1 or guess_col > len(board[0])):
-                print("Oops, that's not even in the ocean.")
-            elif(board[guess_row - 1][guess_col - 1] == "X"):
-                print("You guessed that one already.")
-            else:
-                print("You missed my battleship!")
-            board[guess_row - 1][guess_col - 1] = "X"
-        
-        if each_turn == 3:
-            print ("GAME OVER")
-            print ("My battleship was at the %d column and the %d row." % (ship_col, ship_row) )
+  with term.fullscreen():
+    term.move(0, 0)
+    if last_message:
+      print(last_message)
+    print("Turn", each_turn + 1)
+    print_board(board)
+    
+    guess_row = get_int("Guess Row: ")
+    guess_col = get_int("Guess Column: ")
+
+  if guess_row == ship_row and guess_col == ship_col:
+      last_message ="Congrats, you won!"
+      break
+  else:
+      if (guess_row < 1 or guess_row > len(board)) or (guess_col < 1 or guess_col > len(board[0])):
+          last_message = "Oops, that's not even in the ocean."
+      elif(board[guess_row - 1][guess_col - 1] == "X"):
+          last_message = "You guessed that one already."
+      else:
+          last_message = "You missed my battleship!"
+          board[guess_row - 1][guess_col - 1] = "X"
+  
+  if each_turn == 3:
+    break
+print("GAME OVER")
+print("My battleship was at the %d column and the %d row." % (ship_col, ship_row))
 
