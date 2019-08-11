6034ed02c52445c41b4179cb36ff0601a6712394
Anish Govind <anishg24@gmail.com>
2019-08-11 16:20:40 -0700

    Update battleship.py

--- Extra Fun/battleship.py
+++ Extra Fun/battleship.py
@@ -1,22 +1,24 @@
-import blessings
+from blessings import Terminal as term
 from random import randint
-
-term = blessings.Terminal()
 
 board = []
 
 for x in range(5):
   board.append(["O"] * 5)
 
+
 def print_board(board):
   for row in board:
-    print (" ".join(row))
+    print(" ".join(row))
+
 
 def random_row(board):
   return randint(1, len(board))
 
+
 def random_col(board):
   return randint(1, len(board[0]))
+
 
 def get_int(message):
   answer = input(message)
@@ -30,24 +32,25 @@ def get_int(message):
         answer = input(message)
   return int(answer)
 
+
 ship_row = random_row(board)
 ship_col = random_col(board)
 
 last_message = False
 
 for each_turn in range(4):
-  with term.fullscreen():
-    term.move(0, 0)
+  with term().fullscreen():
+    term().move(0, 0)
     if last_message:
       print(last_message)
     print("Turn", each_turn + 1)
     print_board(board)
-    
+
     guess_row = get_int("Guess Row: ")
     guess_col = get_int("Guess Column: ")
 
   if guess_row == ship_row and guess_col == ship_col:
-      last_message ="Congrats, you won!"
+      print("Congrats, you won!")
       break
   else:
       if (guess_row < 1 or guess_row > len(board)) or (guess_col < 1 or guess_col > len(board[0])):
@@ -57,9 +60,7 @@ for each_turn in range(4):
       else:
           last_message = "You missed my battleship!"
           board[guess_row - 1][guess_col - 1] = "X"
-  
-  if each_turn == 3:
-    break
-print("GAME OVER")
-print("My battleship was at the %d column and the %d row." % (ship_col, ship_row))
 
+print("GAME OVER")
+print("My battleship was at the %d column and the %d row." %
+      (ship_col, ship_row))
